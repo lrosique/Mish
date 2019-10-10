e8c37aefaa4cc04eb6af94030c7bc13c66f01635
lrosique <lambertrosique@hotmail.com>
2019-10-10 18:38:01 +0200

    adds setup files for pip

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mish-activation',
+    version='1.0.0',
+    url='https://github.com/digantamisra98/Mish',
+    packages=find_packages(),
+    author='Diganta Misra',
+    author_email="mishradiganta91@gmail.com",
+    description='Mish activation function package',
+    long_description='A Python implementation of the mish activation function for multiple frameworks.',
+    download_url='https://github.com/digantamisra98/Mish',
+    install_requires=[
+    ],
+)
